e2ade87e35f2d0842f51750867e225e6d67ea95b
elliot <spirityy109@gmail.com>
2014-05-16 18:07:22 +0800

    5.16 update

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,10 +1,46 @@
  (function(){
+
         var trans_dom = $('aside,.main');
         $('.side-icon').click(function(){
-            if(trans_dom.hasClass('trans-show')){
-                trans_dom.removeClass('trans-show').addClass('trans-hide'); 
-            }else{
-                trans_dom.removeClass('trans-hide').addClass('trans-show');
-            }
+            trans_dom.toggleClass('trans-show');
         });
+
+        //$('.side-icon').click();
+
+        $('aside').height($('.main').height());
+
+        //jobcat select init
+        var aside_select_dom =$('.jobcat-selected,.area-selected');
+        
+        aside_select_dom.each(function(k,el){
+            $(el).click(function(){
+                if($(el)){
+                    $(el).find('i').toggleClass('up-arrow');
+                    $(el).next('.'+$(el).attr("class")+'-options').slideToggle('fast');
+                }
+            });
+        
+        });
+
+
+
+
+        //area p select
+        var monidata ={
+            citys : [
+                {city_name:'bj'},
+                {city_name:'sh'},
+                {city_name:'hk'}
+            ]
+        }
+              
+        $('.c-options').html(Handlebars.compile($('.c-options').html())(monidata));
+        
+
+        var  p_options_dom = $('.area-selected-options .p-options'); 
+        p_options_dom.find('li').click(function(){
+            p_options_dom.find('li').removeClass('active');
+            $(this).addClass('active'); 
+        });
+
 })()
