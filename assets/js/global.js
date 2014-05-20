612e04ca917821f67d9226c195210d08eaa18511
elliot <spirityy109@gmail.com>
2014-05-20 15:22:36 +0800

    change js to min and change aside event

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,8 +1,17 @@
  (function(){
 
         var trans_dom = $('aside,.main');
-        $('.side-icon').click(function(){
+        $('.side-icon').click(function(e){
+            e.stopPropagation();
             trans_dom.toggleClass('trans-show');
+            if(trans_dom.hasClass('trans-show')){
+                $('.main.trans-show').on('click',function(e){
+                    e.preventDefault();
+                     $('.side-icon').click();
+                });
+            }else{
+                $('.main').off('click');
+            }
         });
 
         //$('.side-icon').click();
@@ -29,45 +38,52 @@
         var allcitys ={
             '热门':{
                 citys:[
-                    {'city_name':'香港'},
-                    {'city_name':'香港'}
+                    {'city_name':'香港1'},
+                    {'city_name':'香港2'}
                 ]
                 
             },
             '北京':{
                 citys:[
-                    {'city_name':'香港'}
+                    {'city_name':'香港3'}
                 ]
             },
             '上海':{
                 citys:[
-                    {'city_name':'香港'},
-                    {'city_name':'香港'},
-                    {'city_name':'香港'},
+                    {'city_name':'香港4'},
+                    {'city_name':'香港5'},
+                    {'city_name':'香港6'},
                 ]
             }
         };
               
         var  p_options_dom = $('.area-selected-options .p-options'); 
+
         p_options_dom.find('li').click(function(){
+
             p_options_dom.find('li').removeClass('active');
             $(this).addClass('active'); 
+            //handlebars
             $(this).parent('.p-options').next('.c-options').html(Handlebars.compile($('#city-options').html())(allcitys[$(this).text()]));
+
+            AddSelected($(this).parent('.p-options').next('.c-options').find('li'));
+
         });
 
         p_options_dom.find('li').first().click();
 
-
         /*
          * selected action
          */
+        var selected_doms = $('.c-options li,.jobcat-selected-options li');
 
+        function AddSelected(els){
+            els.off('click').click(function(){
+                $(this).closest('.options').prev('div').find('.sub').text($(this).text());
+                $(this).closest('.options').prev('div').click();
+            });
+        }
 
-        /*
-         * bottom layout
-         */
-
-
-
+        AddSelected(selected_doms);
 
 })()
